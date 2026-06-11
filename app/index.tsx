9436c4478a7f9def8640e7ea64282fd107f5f609
wybounty <siberfrancis971@gmail.com>
2026-06-11 10:27:15 -0400

    Tailwind Ajout

--- app/index.tsx
+++ app/index.tsx
@@ -3,15 +3,12 @@ import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/home">Home</Link>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-gray-900">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+      <Link href="/home" className="text-blue-500">Home</Link>
     </View>
   );
 }
+
